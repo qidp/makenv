5c55d845b4ddbe54f2e296ddaaa5469f629eef32
qideping <qideping@makenv.com>
2022-01-10 13:46:46 +0800

    qidp的分支

---
+++ git_exercise/login.py
@@ -0,0 +1,10 @@
+"""
+============================
+author:qideping
+time:2021/11/9 11:24 上午
+E-mail:qideping@makenv.com
+============================
+"""
+from selenium import webdriver
+
+driver = webdriver.Chromegit 
